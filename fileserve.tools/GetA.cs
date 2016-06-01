80fca005fb7b51ebb47022efcb1a499bb8d444de
Tom Postler <tom@postler.me>
2016-06-01 16:05:41 -0500

    fileserve.tools

---
+++ fileserve.tools/GetA.cs
@@ -0,0 +1,50 @@
+﻿namespace Unlimitedinf.Fileserve.Tools
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Security;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Contains various static methods to get information from the user.
+    /// </summary>
+    public static class GetA
+    {
+        /// <summary>
+        /// Get a password from the console as a secure string, printing asteriks instead of the user's password while
+        /// they type it in.
+        /// </summary>
+        /// <remarks>
+        /// A la http://stackoverflow.com/a/3404464
+        /// </remarks>
+        /// <returns></returns>
+        public static SecureString PasswordFromConsole()
+        {
+            SecureString pwd = new SecureString();
+            while (true)
+            {
+                ConsoleKeyInfo i = Console.ReadKey(true);
+                if (i.Key == ConsoleKey.Enter)
+                {
+                    break;
+                }
+                else if (i.Key == ConsoleKey.Backspace)
+                {
+                    if (pwd.Length > 0)
+                    {
+                        pwd.RemoveAt(pwd.Length - 1);
+                        Console.Write(Properties.Resources.GetAPasswordBackspace);
+                    }
+                }
+                else if (!char.IsControl(i.KeyChar))
+                {
+                    pwd.AppendChar(i.KeyChar);
+                    Console.Write(Properties.Resources.GetAPasswordCoverChar);
+                }
+            }
+            return pwd;
+        }
+    }
+}
