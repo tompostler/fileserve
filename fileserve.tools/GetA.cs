f82fbb22b38e96c6d7c7d1732adfcba9b1b08e54
Tom Postler <tom@postler.me>
2016-06-06 16:13:19 -0500

    Password tools.

--- fileserve.tools/GetA.cs
+++ fileserve.tools/GetA.cs
@@ -4,6 +4,7 @@
     using System.Collections.Generic;
     using System.Linq;
     using System.Security;
+    using System.Security.Cryptography;
     using System.Text;
     using System.Threading.Tasks;
 
@@ -20,7 +21,7 @@
         /// A la http://stackoverflow.com/a/3404464
         /// </remarks>
         /// <returns></returns>
-        public static SecureString PasswordFromConsole()
+        public static SecureString PasswordFromConsoleSecurely()
         {
             SecureString pwd = new SecureString();
             while (true)
@@ -46,6 +47,56 @@
                 }
             }
             return pwd;
+        }
+
+        /// <summary>
+        /// Get a password from the console, printing asteriks instead of the user's password while they type it in.
+        /// </summary>
+        /// <remarks>
+        /// A la http://stackoverflow.com/a/3404464
+        /// </remarks>
+        /// <returns></returns>
+        public static string PasswordFromConsole()
+        {
+            StringBuilder pwd = new StringBuilder();
+            while (true)
+            {
+                ConsoleKeyInfo i = Console.ReadKey(true);
+                if (i.Key == ConsoleKey.Enter)
+                {
+                    Console.WriteLine();
+                    break;
+                }
+                else if (i.Key == ConsoleKey.Backspace)
+                {
+                    if (pwd.Length > 0)
+                    {
+                        pwd.Remove(pwd.Length - 1, 1);
+                        Console.Write(Properties.Resources.GetAPasswordBackspace.Replace("\\b", "\b"));
+                    }
+                }
+                else if (!char.IsControl(i.KeyChar))
+                {
+                    pwd.Append(i.KeyChar);
+                    Console.Write(Properties.Resources.GetAPasswordCoverChar);
+                }
+            }
+            return pwd.ToString();
+        }
+
+        /// <summary>
+        /// Get a base64 token.
+        /// </summary>
+        /// <param name="length"></param>
+        /// <returns></returns>
+        public static string Token(int length = 64)
+        {
+            byte[] random = new byte[length * 3 / 4 + 1];
+            using (RNGCryptoServiceProvider rngCsp = new RNGCryptoServiceProvider())
+            {
+                rngCsp.GetBytes(random);
+            }
+            return Convert.ToBase64String(random).Substring(0, length);
         }
     }
 }
---
+++ fileserve.tools/Password.cs
@@ -0,0 +1,61 @@
+﻿namespace Unlimitedinf.Fileserve.Tools
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Contains various static methods to handle passwords.
+    /// </summary>
+    public static class Password
+    {
+        /// <summary>
+        /// Using BCrypt, hash a user's password. Work factor of 12.
+        /// </summary>
+        /// <param name="password"></param>
+        /// <param name="workFactor"></param>
+        /// <returns>A fixed 60-character BCrypt hash.</returns>
+        public static string Hash(string password, int workFactor = 12)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password, workFactor);
+        }
+
+        /// <summary>
+        /// Validate a password matches for a hash.
+        /// </summary>
+        /// <param name="password"></param>
+        /// <param name="hash"></param>
+        /// <returns></returns>
+        public static bool Validate(string password, string hash)
+        {
+            return BCrypt.Net.BCrypt.Verify(password, hash);
+        }
+
+        /// <summary>
+        /// Given a password and its hash, will upgrade the hash's work factor by a certain amount.
+        /// </summary>
+        /// <param name="password"></param>
+        /// <param name="hash"></param>
+        /// <param name="levels"></param>
+        /// <returns></returns>
+        public static string HashUpgrade(string password, string hash, int levels = 1)
+        {
+            if (Password.Validate(password, hash))
+            {
+                return Password.Hash(password, levels);
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// Wrapper for <see cref="GetA.PasswordFromConsole"/>.
+        /// </summary>
+        /// <returns></returns>
+        public static string GetFromConsole()
+        {
+            return GetA.PasswordFromConsole();
+        }
+    }
+}
