e6ac91f70290fc1e7111f127841a8325b37d4801
Tom Postler <tom@postler.me>
2016-06-07 22:41:58 -0500

    config file construction.

--- fileserve/Config/Config.cs
+++ fileserve/Config/Config.cs
@@ -1,8 +1,10 @@
 ﻿namespace Unlimitedinf.Fileserve.Config
 {
     using Json;
+    using Newtonsoft.Json;
     using System;
     using System.Collections.Generic;
+    using System.IO;
     using System.Linq;
     using System.Text;
     using System.Threading.Tasks;
@@ -13,8 +15,9 @@
     internal sealed partial class Config
     {
         private readonly List<User> users;
-        private readonly List<File> files;
+        private readonly List<Json.File> files;
         private readonly Dictionary<Guid, HashSet<Guid>> userfiles;
+        private readonly string filename;
 
         /// <summary>
         /// Default Ctor.
@@ -22,19 +25,34 @@
         public Config()
         {
             this.users = new List<User>();
-            this.files = new List<File>();
+            this.files = new List<Json.File>();
             this.userfiles = new Dictionary<Guid, HashSet<Guid>>();
+            this.filename = "fileserve.json";
         }
 
         /// <summary>
-        /// Ctor to use when reading the configuration from a file.
+        /// Ctor when given a filename.
         /// </summary>
-        /// <param name="fileConfig"></param>
-        public Config(Overall fileConfig)
+        /// <param name="filename"></param>
+        public Config(string filename) : this()
         {
-            this.users = fileConfig.Users;
-            this.files = fileConfig.Files;
-            this.userfiles = fileConfig.UserFiles;
+            this.filename = filename;
+
+            if (System.IO.File.Exists(this.filename))
+            {
+                Overall configFile;
+
+                using (StreamReader sr = new StreamReader(filename))
+                using (JsonReader jr = new JsonTextReader(sr))
+                {
+                    JsonSerializer js = new JsonSerializer();
+                    configFile = js.Deserialize<Overall>(jr);
+                }
+
+                this.files = configFile.Files;
+                this.users = configFile.Users;
+                this.userfiles = configFile.UserFiles;
+            }
         }
     }
 }
